509bfa4dabfaa0d49048d01908494a242da229eb
XShadeSlayerXx <xshadeslayerxx@gmail.com>
2025-02-17 01:09:19 -0600

    Fixed shuffle not working. Only server host needs the mod

--- random_teams.cs
+++ random_teams.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
@@ -23,36 +24,6 @@ public class RandomTeams : BaseUnityPlugin
         harmony.PatchAll();
     }
 
-    [HarmonyPatch(typeof(PrivateRoomCharacterSelectionInstance), "RPCO_SelectFace")]
-    class RPC_Hijack_Patch
-    {
-        static bool Prefix(ref PrivateRoomCharacterSelectionInstance __instance, int faceID, int eyeID)
-        {
-            //faceID is uniqueID, eyeID is colorID
-            if (faceID > -1) { return true; } //don't intercept the call if the faceID is valid
-            if (__instance.currentPlayer.uniqueID != faceID) return false; //discard, not meant for us
-
-            ChangeTeam_Override(ref __instance, faceID, eyeID);
-            return false;
-        }
-    }
-
-    public static void ChangeTeam_Override(ref PrivateRoomCharacterSelectionInstance instance, int uniqueID, int newColorID)
-    {
-        //if (uniqueID != __instance.uniqueID) return; //not directed at us
-        instance.lastChangedTeams = Time.realtimeSinceStartup;
-
-        instance.colorID = newColorID;
-
-        LobbyCharacter character = PrivateRoomHandler.instance.FindLobbyCharacter(uniqueID);
-        character.colorID = newColorID;
-        LobbyCharacter[] characters = PhotonNetwork.CurrentRoom.Players.Values.ToList().Select(p => p.GetProperty<LobbyCharacter[]>("players")).SelectMany(p => p).Where(p => p != null).ToArray();
-        characters[character.localID] = character;
-        PhotonNetwork.LocalPlayer.SetProperty("players", characters);
-
-        instance.UpdateFaceColors();
-    }
-
     [HarmonyPatch(typeof(PrivateRoomCharacterSelectionInstance), nameof(Update))]
     class CharacterSelect_Update_Patch
     {
@@ -63,68 +34,100 @@ public class RandomTeams : BaseUnityPlugin
             {
                 return;
             }
-            int zero = 0;
-            Vector2 v_zero = new(0, 0);
 
             //if (pressed a numrow button)
             if (
                 Input.GetKeyDown(KeyCode.Alpha1) ||
                 Input.GetKeyDown(KeyCode.Alpha2) ||
                 Input.GetKeyDown(KeyCode.Alpha3) ||
-                Input.GetKeyDown(KeyCode.Alpha4)
+                Input.GetKeyDown(KeyCode.Alpha4) ||
+                Input.GetKeyDown(KeyCode.Alpha5) ||
+                Input.GetKeyDown(KeyCode.Alpha6) ||
+                Input.GetKeyDown(KeyCode.Alpha7) ||
+                Input.GetKeyDown(KeyCode.Alpha8) ||
+                Input.GetKeyDown(KeyCode.Alpha9)
                )
             {
-                PhotonView view = __instance.gameObject.GetComponent<PhotonView>();
-
                 //we're the host/master client
-                LobbyCharacter[] characters = PhotonNetwork.LocalPlayer.GetProperty<LobbyCharacter[]>("players").Where(p => p != null).ToArray();
+                LobbyCharacter[] characters = PhotonNetwork.CurrentRoom.Players.Values.ToList().Select(p => p.GetProperty<LobbyCharacter[]>("players")).SelectMany(p => p).Where(p => p != null).ToArray();
 
                 int playersPerTeam = 2; //set to the number that was pressed. initialize it to 2 first.
                 if (Input.GetKeyDown(KeyCode.Alpha1))      { playersPerTeam = 1; }
                 else if (Input.GetKeyDown(KeyCode.Alpha2)) { playersPerTeam = 2; }
                 else if (Input.GetKeyDown(KeyCode.Alpha3)) { playersPerTeam = 3; }
                 else if (Input.GetKeyDown(KeyCode.Alpha4)) { playersPerTeam = 4; }
+                else if (Input.GetKeyDown(KeyCode.Alpha5)) { playersPerTeam = 5; }
+                else if (Input.GetKeyDown(KeyCode.Alpha6)) { playersPerTeam = 6; }
+                else if (Input.GetKeyDown(KeyCode.Alpha7)) { playersPerTeam = 7; }
+                else if (Input.GetKeyDown(KeyCode.Alpha8)) { playersPerTeam = 8; }
+                else if (Input.GetKeyDown(KeyCode.Alpha9)) { playersPerTeam = 9; }
 
                 //int numCharacters = characters.Length; //necessary if we want # of teams instead
                 //int maxColors = RWFMod.MaxColorsHardLimit;
                 int counter = -1;
-                int color = 0;
-                foreach (var character in characters.OrderBy(_ => UnityEngine.Random.Range(0, 1)))
+                int colorIndex = 0;
+                List<int> colors = new(Enumerable.Range(0, RWFMod.MaxColorsHardLimit));
+
+                //randomize the team colors so it's not just `orange -> blue -> etc` every time
+                colors.Shuffle();
+                characters.Shuffle();
+                foreach (var character in characters)
                 {
-                    if (counter++ == playersPerTeam)
+                    counter++;
+                    if (counter == playersPerTeam)
                     {
                         counter = 0;
-                        if (color++ == RWFMod.MaxColorsHardLimit)
+                        colorIndex++;
+                        if (colorIndex == RWFMod.MaxColorsHardLimit)
                         {
-                            color = 0;
+                            colorIndex = 0;
                         }
                     }
-                    character.colorID = color;
                     //Logger.LogInfo("Randomized " + character.actorID + " to: " + character.colorID);
-                    object[] test = { character.uniqueID, character.colorID, v_zero, zero, v_zero, zero, v_zero, zero, v_zero };
-                    view.RPC("RPCO_SelectFace", RpcTarget.All, test);
+                    __instance.SendRPC_ChangeIDToTeam(character.uniqueID, colors[colorIndex]);
                 }
-
-                //PhotonNetwork.LocalPlayer.SetProperty("players", characters);
-                //__instance.UpdateFaceColors();
             }
 
             //randomize all players
             else if (Input.GetKeyDown(KeyCode.Alpha0))
             {
-                PhotonView view = __instance.gameObject.GetComponent<PhotonView>();
                 LobbyCharacter[] characters = PhotonNetwork.CurrentRoom.Players.Values.ToList().Select(p => p.GetProperty<LobbyCharacter[]>("players")).SelectMany(p => p).Where(p => p != null).ToArray();
 
                 foreach (var character in characters)
                 {
                     int random_color = UnityEngine.Random.RandomRangeInt(0, RWFMod.MaxColorsHardLimit);
-                    character.colorID = random_color;
+                    //character.colorID = random_color;
                     //Logger.LogWarning("Randomized " + character.actorID + " to: " + random_color);
 
-                    object[] test = { character.uniqueID, random_color, v_zero, zero, v_zero, zero, v_zero, zero, v_zero };
-                    view.RPC("RPCO_SelectFace", RpcTarget.All, test);
+                    __instance.SendRPC_ChangeIDToTeam(character.uniqueID, random_color);
                 }
             }
+        }
+    }
+}
+
+public static class PrivateRoomCharacterSelectionInstance_Extensions
+{
+    public static void SendRPC_ChangeIDToTeam(this PrivateRoomCharacterSelectionInstance instance, int uniqueID,  int colorID)
+    {
+        var character = PrivateRoomHandler.instance.FindLobbyCharacter(uniqueID);
+        var characterSelectorPhotonView = PrivateRoomHandler.instance.versusDisplay.PlayerSelectorGO(character.uniqueID).GetComponent<PhotonView>();
+        characterSelectorPhotonView.RPC(nameof(PrivateRoomCharacterSelectionInstance.RPCA_ChangeTeam), RpcTarget.All, colorID);
+    }
+}
+
+public static class ListExtension
+{
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        var count = list.Count;
+        var last = count - 1;
+        for ( var i = 0; i < last; i++ )
+        {
+            var r = UnityEngine.Random.Range(i, count);
+            var tmp = list[i];
+            list[i] = list[r];
+            list[r] = tmp;
         }
     }
 }
